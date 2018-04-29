4e2e28d28ee50d427d4fd3b285851a00dc2e0664
ziewkaa <eva.zientara@gmail.com>
2018-04-30 00:23:38 +0200

    Adds interface

--- src/main/java/pl/coderslab/model/BookService.java
+++ src/main/java/pl/coderslab/model/BookService.java
@@ -1,4 +1,13 @@
 package pl.coderslab.model;
 
+import java.util.List;
+
 public interface BookService {
+
+    public List<Book> getList();
+    public Book getBook(int id);
+    public void addBook( String isbn, String title, String author, String publisher, String type);
+    public void updateBook(int id, String isbn, String title, String author, String publisher, String type );
+    public void deleteBook(int id );
+
 }
